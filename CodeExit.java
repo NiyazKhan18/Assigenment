789260a28e4c54fe352f8d000cdd6fe7ba08d475
NiyazKhan18 <86282483+NiyazKhan18@users.noreply.github.com>
2021-06-29 23:20:46 +0530

    Add files via upload

---
+++ CodeExit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class CodeExit
+{
+  public void out()
+  {
+    System.out.println("\n*****************************************");
+    System.out.println(" Thank You For Using my Software :) ");
+    System.out.println("*****************************************");
+    System.exit(0);
+  }
+}
---
+++ MainMenu.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class MainMenu
+{
+  public void menu()
+  {
+    System.out.println("*******************************************");
+    System.out.println("\t  EMPLOYEE MANAGEMENT SYSTEM");
+    System.out.println("*******************************************");
+    System.out.println("\t\t\t--------------------");
+    System.out.println("\t\t\t ~$ Niyaz Khan");
+    System.out.println("\t\t\t--------------------");
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t    --------------------");
+    System.out.println("\t\t\t     ~$ Niyaz Khan");
+    System.out.println("\t\t\t    --------------------");
+    System.out.println("\n\nPress 1 : To Add an Employee Details");
+    System.out.println("Press 2 : To See an Employee Details ");
+    System.out.println("Press 3 : To Remove an Employee");
+    System.out.println("Press 4 : To Update Employee Details");
+    System.out.println("Press 5 : To Exit the EMS Portal");
+  }
+}
